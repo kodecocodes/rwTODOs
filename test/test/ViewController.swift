5d7f575f79b145f85665886809ae2f374ee8f0fa
vladimir-pravdivy <vpravdivyi@netpulse.com>
2021-06-30 14:53:17 +0300

    1

---
+++ test/test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by pravdivy on 30.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
